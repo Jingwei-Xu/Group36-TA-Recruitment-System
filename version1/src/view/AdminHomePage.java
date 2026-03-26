a3032bc05e084788ec9cec04b4aac0ee3e400416
ahuo-ahuo <dpbla@icloud.com>
2026-03-26 22:29:10 +0800

    Create AdminHomePage.java

---
+++ version1/src/view/AdminHomePage.java
@@ -0,0 +1,8 @@
+package view;
+
+public class AdminHomePage {
+    public static void show() {
+        System.out.println("\n=== Admin Portal ===");
+        System.out.println("Welcome, Admin user!");
+    }
+}
