f691eaafcf48794d8da64d93756f408bb959272f
ahuo-ahuo <dpbla@icloud.com>
2026-03-26 22:49:02 +0800

    Update RegisterPage.java

--- version1/src/view/RegisterPage.java
+++ version1/src/view/RegisterPage.java
@@ -1,3 +1,5 @@
+package view;
+
 import java.util.Scanner;
 import controller.AuthController;
 
@@ -7,16 +9,38 @@ public class RegisterPage {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\n===== REGISTER PAGE =====");
+        System.out.println("\n=== Register ===");
 
-        System.out.print("Enter username: ");
+        System.out.print("Username: ");
         String username = scanner.nextLine();
 
-        System.out.print("Enter password: ");
+        System.out.print("Password: ");
         String password = scanner.nextLine();
 
-        System.out.print("Enter role (TA / MO / Admin): ");
-        String role = scanner.nextLine();
+        System.out.println("Select Role:");
+        System.out.println("1. TA");
+        System.out.println("2. MO");
+        System.out.println("3. Admin");
+
+        int roleChoice = scanner.nextInt();
+        scanner.nextLine();
+
+        String role = "";
+
+        switch (roleChoice) {
+            case 1:
+                role = "TA";
+                break;
+            case 2:
+                role = "MO";
+                break;
+            case 3:
+                role = "Admin";
+                break;
+            default:
+                System.out.println("Invalid role.");
+                return;
+        }
 
         controller.handleRegister(username, password, role);
     }
